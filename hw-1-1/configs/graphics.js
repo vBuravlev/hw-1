4748b6b45feb2cafe4e44ad4a8aed1e168c5fbed
Masta <masta54@mail.ru>
2022-12-29 08:39:22 +0700

    очистка мастера

--- hw-1-1/configs/graphics.js
+++
@@ -1,7 +0,0 @@
-
-module.exports = {
-    LAST: '└',
-    MID: '├',
-    LONG: '|',
-    TAB: ' '
-}
--- hw-1-2/utils/fs-helpers.js
+++
@@ -1,8 +0,0 @@
-
-const fsPromises = require('node:fs/promises');
-
-const readDir = async (dirpath) => {
-    return await fsPromises.readdir(dirpath, { withFileTypes: true });
-}
-
-module.exports = { readDir };
